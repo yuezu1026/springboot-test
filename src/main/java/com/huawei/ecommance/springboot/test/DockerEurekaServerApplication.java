a91a67f854745749688198c98f8eb3dae66d7835
yuezu1026 <yuezu1026@sina.com>
2019-07-13 18:07:41 +0800

    add

---
+++ src/main/java/com/huawei/ecommance/springboot/test/DockerEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.huawei.ecommance.springboot.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerEurekaServerApplication {
+
+	public static void main(String[] args) {
+		System.out.println("sdfdsf");
+		SpringApplication.run(DockerEurekaServerApplication.class, args);
+	}
+}
