d72a0a5ced1f46cc93c46f843e8f20b8fc59df8f
yuezu1026 <yuezu1026@sina.com>
2019-07-20 09:56:55 +0800

    add

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM java:8
 VOLUME /tmp
-ADD ./target/gs-spring-boot-docker-master-0.0.1-SNAPSHOT.jar app.jar 
+ADD target/gs-spring-boot-docker-master-0.0.1-SNAPSHOT.jar app.jar 
 RUN sh -c 'touch /app.jar'
 ENV JAVA_OPTS=""
 ENTRYPOINT [ "sh", "-c", "java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /app.jar" ]
--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,9 +1,6 @@
 package hello;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.utils.CassandraLink;
 
 @SpringBootApplication
 @RestController
@@ -16,7 +13,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		// CassandraLink.link();
+		//CassandraLink.link();
 	}
 
 }
